bfe2a757fdb088b486d64a62e538a2b2da4fdc86
adentrekin <adentrekin@mix.wvu.edu>
2017-04-04 20:33:01 -0400

    Overhaul Assignment11
    Gave more responsibility to Store class
    
    Divided work into smaller functions
    
    Refactored functions to throw exceptions rather than return null.
    
    See complementary documentation on Google Doc

--- Assignment11/src/Item.java
+++ Assignment11/src/Item.java
@@ -21,18 +21,6 @@ public class Item {
 	}
 	
 	public static Item bundle(Item foo, Item bar){
-		//Create a new item
-		//The new item's name is foo's name + bar's name
-		//The new item's cost is foo's cost + bar's cost
-		//If either foo or bar is null, throw a NullPointerException describing which is null
-		
-		
-		//Solution code TODO delete for students
-		if (foo == null){
-			throw new NullPointerException("Null Item Foo");
-		}else if (bar == null){
-			throw new NullPointerException("Null Item Bar");
-		}
 		return new Item(foo.name + " + " + bar.name, foo.cost + bar.cost);
 		
 	}
--- Assignment11/src/Store.java
+++ Assignment11/src/Store.java
@@ -6,89 +6,143 @@ import java.util.Scanner;
 
 
 public class Store {
-	public static Item readItem(Scanner source){
+	
+	Item[] itemsForSale;
+	
+	
+	public Store(String fileName) throws FileNotFoundException{
+		//Creates a store based off of a local file
 		
 		/*
-		 * Read an Item from the scanner
+		 *  Use a scanner to read from the file with the name, fileName
+		 * 
+		 * Read the first int from the file
+		 *   This represents the maximum number of Items to read
+		 *   i.e. the size of array you'll need for the next step
+		 *   
+		 * Instantiate the array itemsForSale
+		 *   Use the number read from the file as the size of the array
+		 *   
+		 * Read items from the file until you get a NoSuchElementException or the array is full
+		 *   if you get a NumberFormatException:
+		 *     print 'Bad Line'
+		 *     continue reading from the file
+		 *   Save each item to the array itemsForSale
+		 *   
+		 * If itemsForSale isn't full, resize it so that it has no null elements
+		 */
+		
+		//TODO remove solution
+		
+		Scanner source = new Scanner(new File("catalog.txt"));
+		int size = source.nextInt();
+		
+		itemsForSale = new Item[size];
+		boolean end = false;
+		int actualItemCount = 0;
+		while(actualItemCount < size && !end){
+			try{
+				itemsForSale[actualItemCount] = readItem(source);
+				actualItemCount++;
+			}catch(NoSuchElementException e){
+				end = true;
+			}catch(NumberFormatException e){
+				System.out.println("Bad Line");
+			}
+		}
+		itemsForSale = Arrays.copyOf(itemsForSale, actualItemCount);
+		
+		
+		
+	}
+	
+	
+	public static Item readItem(Scanner source)throws NoSuchElementException, NumberFormatException{
+		/*
+		 * Read an Item from the scanner, and return that item.
+		 * 
 		 * Items are stored as a two word pair
 		 * The first word is the name
+		 *  See if the scanner has another word
+		 *   If it does, the next word is the Item's name
+		 *   if it doesn't, throw a NoSuchElementException
+		 *   
 		 * The second word is the price
-		 * Use the Double.parseDouble to convert from String to double
-		 * 
-		 * Catch the following exceptions
-		 * NoSuchElementException
-		 *  Happens when you reach the end of the file prematurely
-		 *  Print out "End of file" and return null
-		 * NumberFormatException
-		 *  Happens when you can't convert a String to a double
-		 *  Dispose of the file's current line by using nextLine
-		 *  Print "Malformed Input, line disposed"
-		 *  return null
+		 *   If the scanner has another double
+		 *     The next double is the Item's price
+		 *     Construct the item and return it
+		 *   If the scanner has another word but not a double
+		 *     throw away that line of input using nextLine
+		 *     throw a NumberFormatException
+		 *   Otherwise
+		 *     throw a NoSuchElement exception
 		 */
 		
 		//Solution. TODO remove before providing to students
-		try{
-			String name = source.next();
-			String priceS = source.next();
-			double price = Double.parseDouble(priceS);
-			return new Item(name, price);
+		if(source.hasNext()){
+			String s = source.next();
+			if(source.hasNextDouble()){
+				double price = source.nextDouble();
+				return new Item(s, price);
+			}
+			else if (source.hasNext()){
+				source.nextLine();
+				throw new NumberFormatException();
+			}
 		}
-		//Seems a little cumbersome.
-		//Maybe we should use input mismatch rather than
-		// encourage reading numerical data as a string?
-		catch(NumberFormatException e){
-			String eat = source.nextLine();
-			System.out.println("Malformed Input, line disposed: " + eat);
-			return null;
-		} catch(NoSuchElementException e){
-			System.out.println("End of File");
-			return null;
-		}
+		throw new NoSuchElementException();
+		
 	}
 		
+	public static Item[] makeBundles(Item[] baseItems){
+		/*
+		 * Using the bundle method, create every possible two-item bundle
+		 * Return an array containing every possible two-time bundle
+		 * This array should be *full*, i.e. no null elements
+		 * 
+		 * You'll definitely need nested loops to fill the new array
+		 * BONUS calculate the size of the array without using a loop
+		 * Otherwise, a loop may be useful.
+		 */
+		
+		//Solution TODO Remove solution
+		int size = (baseItems.length -1) * baseItems.length / 2;
+		Item[] combos = new Item[size];
+		int index = 0;
+		for(int i = 0; i < baseItems.length; i++){
+			for(int j =i + 1; j <baseItems.length; j++){
+				combos[index] = Item.bundle(baseItems[i], baseItems[j]);
+				index++;
+			}
+		}
+		return combos;
+	}
+	
 	
 	public static void main(String args[]){
 		
 		/*
-		 * Use a scanner to read from the file 'catalog.txt'
-		 * Read the first int from the file, that's how many Items the file contains
-		 * Create an array of Items
-		 * Copy items from the file into the array using a loop and readItem
-		 * Attempt to make every possible two-item bundle using nested loops and the bundle method
-		 *   If the bundle method throws an exception, print the message
-		 *   If the bundle method doesn't throw an exception, print the bundle
+		 * Create a Store from the file 'catalog.txt'
+		 *   if you get a FileNotFoundException:
+		 *     Print "File 'catalog.txt' not found"
+		 *     return
 		 * 
-		 * if the file doesn't exist
-		 *  print 'Catalog not found;
-		 *  quit
+		 * Generate every possible two-item bundle using makeBundles
 		 * 
+		 * Print out each bundle on its own line
 		 */
 		
 		//Solution. TODO remove before providing to students
-		Scanner source = null;
-		int size = 0;
+		Store store = null;
 		try{
-			source = new Scanner(new File("catalog.txt"));
-			size = source.nextInt();
+			store = new Store("catalog.txt");
 		}catch(FileNotFoundException f){
-			System.out.println("Catalog not found");
-			return;
-		}catch(NumberFormatException e){
-			System.out.println("Bad format: number of items not found");
+			System.out.println("File 'catalog.txt' not found");
 			return;
 		}
-		Item[] catalog = new Item[size];
-		for(int i = 0; i < size; i++){
-			catalog[i] = readItem(source);
-		}
-	    for(int i = 0; i < size; i++){
-	    	for(int j = i + 1; j < size; j++){
-	    		try{
-	    			System.out.println(Item.bundle(catalog[i], catalog[j]));
-	    		}catch(NullPointerException e){
-	    			System.out.println(e.getMessage());
-	    		}
-	    	}
-	    }
+	
+	   for(Item e: makeBundles(store.itemsForSale)){
+		   System.out.println(e);
+	   }
 	}
 }
